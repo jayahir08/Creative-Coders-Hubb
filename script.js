b6331fa41a15020b0a826aec217e3b1880b91bfc
JAY AHIR <a.j.makwana715@gmail.com>
2025-11-12 15:56:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener("contextmenu", function(e){
+    e.preventDefault();
+  });
+  document.onkeydown = function(e) {
+    if (e.keyCode == 123) return false; // F12 disable
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
+  }
+</script>
