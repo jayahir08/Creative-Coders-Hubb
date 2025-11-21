b345ca4125213dea6a87d15a6221749160f32279
JAY AHIR <a.j.makwana715@gmail.com>
2025-11-21 10:41:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 <script>
-  document.addEventListener("contextmenu", function(e){
-    e.preventDefault();
-  });
-  document.onkeydown = function(e) {
-    if (e.keyCode == 123) return false; // F12 disable
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
-  }
+  // document.addEventListener("contextmenu", function(e){
+  //   e.preventDefault();
+  // });
+  // document.onkeydown = function(e) {
+  //   if (e.keyCode == 123) return false; // F12 disable
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
+  //   if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
+  // }
 </script>
+
